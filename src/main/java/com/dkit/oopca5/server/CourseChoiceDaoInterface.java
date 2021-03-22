1b32d8a95a0b81b3428fd136ad65053e4abc28b6
seanmcavoy <smcavoy.mcavoy@gmail.com>
2021-03-22 20:51:28 +0000

    DISPLAY_CURRENT_CHOICES() created

--- src/main/java/com/dkit/oopca5/server/CourseChoiceDaoInterface.java
+++ src/main/java/com/dkit/oopca5/server/CourseChoiceDaoInterface.java
@@ -1,5 +1,11 @@
 package com.dkit.oopca5.server;
 
+import com.dkit.oopca5.Exceptions.DaoException;
+import com.dkit.oopca5.core.Course;
+
+import java.util.List;
+
 public interface CourseChoiceDaoInterface
 {
+    public List<Course> displayCurrentChoices() throws DaoException;
 }
--- src/main/java/com/dkit/oopca5/server/MySqlCourseChoice.java
+++ src/main/java/com/dkit/oopca5/server/MySqlCourseChoice.java
@@ -1,6 +1,15 @@
 package com.dkit.oopca5.server;
 
+import com.dkit.oopca5.Exceptions.DaoException;
+import com.dkit.oopca5.core.Course;
+
+import java.util.List;
+
 public class MySqlCourseChoice extends MySqlDAO implements CourseChoiceDaoInterface
 {
+    public List<Course> displayCurrentChoices() throws DaoException
+    {
+        return null;
+    }
 
 }
